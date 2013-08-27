c2b9bcb5c931f89dcbbf473cefe8b238e01e5429
zniper <me.zniper@gmail.com>
2013-08-27 19:23:25 +0700

    Add script for correcting language

--- vpr/tests/migrate/shell_migrate.py
+++ vpr/tests/migrate/shell_migrate.py
@@ -32,3 +32,32 @@ def removeDuplicatedTitleInMaterial():
             print 'Updating failed at ' + str(mid)
 
 
+
+def determineLanguage(title, text):
+    VI_ELEMENTS_0 = (u'\u1ea1', u'\u1ebf', u'\u01b0')
+    VI_ELEMENTS_1 = ('&#226;', '&#432;', '&#224;')
+    for item in VI_ELEMENTS_0:
+        if item in title:
+            return 'vi'
+    for item in VI_ELEMENTS_1:
+        if item in text:
+            return 'vi'
+    return 'en'
+
+
+def correctMaterialLanguage(material, dry=False):
+    if type(material) == str:
+        material = models.Material.objects.get(material_id=material)
+
+    new_lang = determineLanguage(material.title, material.text)
+    print str(material.language) + ' > ' + new_lang
+    
+    if not dry:
+        material.language = new_lang 
+        material.save()
+
+
+def correctAllLanguages(dry=True):
+    targets = models.Material.objects.exclude(language='vi')
+    for material in targets:
+        correctMaterialLanguage(material, dry)
