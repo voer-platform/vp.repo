5b6e10ac5fc229f4b8ab674cf1d63bfc19f3c066
zniper <me.zniper@gmail.com>
2013-09-05 15:42:11 +0700

    Add missing profile

---
+++ vpr/vpr/settings/staging.py
@@ -0,0 +1,24 @@
+# Django settings for vpr project.
+
+from base import *
+
+DEBUG = TEMPLATE_DEBUG = False 
+
+DATABASES = {
+    'default': {
+        #'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        #'NAME': 'vpr.sqlite3',                  # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.mysql',    # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'vpr',                      # Or path to database file if using sqlite3.
+        'USER': 'vpr',                      # Not used with sqlite3.
+        'PASSWORD': 'vpr',                  # Not used with sqlite3.
+        'HOST': 'localhost',                # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                     # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+ROOT_URLCONF = 'vpr.urls.prod'
+
+VPT_URL = 'http://dev.voer.vn:6543/'
+
+TOKEN_REQUIRED = False 
