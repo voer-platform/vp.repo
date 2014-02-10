c0493e466cc320ccce55b80906a3ab904d2e9408
Zniper <me.zniper@gmail.com>
2014-02-10 10:56:38 +0700

    Add TinyMCE to text fields in admin area (for Mateiral only)

--- vpr/vpr_content/admin.py
+++ vpr/vpr_content/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 import models
 
-admin.site.register(models.Material)
+class MaterialAdmin(admin.ModelAdmin):
+
+    class Media:
+        js = [
+            '/s/grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js',
+            '/s/grappelli/tinymce_setup/tinymce_setup.js',
+        ]
+
+#admin.site.register(models.Material)
+admin.site.register(models.Material, MaterialAdmin)
 admin.site.register(models.Category)
 admin.site.register(models.Person)
 admin.site.register(models.MaterialFile)
