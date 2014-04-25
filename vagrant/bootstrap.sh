c87f02bc56e4e3a4e2d1809341f9ff05ea5fe912
Zniper <me.zniper@gmail.com>
2014-04-25 13:24:46 +0700

    Complete the bootstrap script of VPR

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -5,7 +5,11 @@ apt-get install -y git python-virtualenv python-dev python-software-properties
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
 add-apt-repository 'deb http://download.nus.edu.sg/mirror/mariadb/repo/5.5/debian wheezy main'
 apt-get update -y
+export DEBIAN_FRONTEND=noninteractive
+debconf-set-selections <<< 'mariadb-server-5.5 mysql-server/root_password password'
+debconf-set-selections <<< 'mariadb-server-5.5 mysql-server/root_password_again password'
 apt-get install -y mariadb-server mariadb-client libmariadbclient-dev
+#/etc/init.d/mysql start
 
 mysql -u root -e "create database vpr;"
 mysql -u root -e "grant all privileges on vpr.* to 'vpr'@'localhost' identified by 'vpr';"
@@ -18,3 +22,9 @@ source bin/activate
 git clone https://github.com/voer-platform/vp.repo.git
 cd vp.repo
 sh install.sh
+mysql vpr -uvpr -pvpr -e "INSERT INTO django_site (domain, name) VALUES ('localhost', 'localhost');"
+
+chown vagrant.vagrant -R /home/vagrant/vpr
+
+cd vpr
+python manage.py runserver 0:2013 &
